d1149093367fa5a744e15cf13d97d9827140494b
masutaniR <popohu87@gmail.com>
2020-12-19 08:06:49 +0000

    [Add] 4-10 サクセスメッセージ（ログイン関係以外）

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice: 'You have created book successfully.'
     else
       @books = Book.all
       render :index
@@ -22,12 +22,17 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book)
+      redirect_to book_path(@book), notice: 'You have updated book successfully.'
     else
       render :edit
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,17 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to current_user
+    end
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(@user.id)
+      redirect_to user_path(@user.id), noticeUser: 'You have updated user successfully.'
     else
       render :edit
     end
