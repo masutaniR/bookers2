269a0d7f1f66a7eafa36b53aa69cbbf69a7faf35
masutaniR <popohu87@gmail.com>
2020-12-19 05:48:22 +0000

    [Add] 4-10 ログイン・ログアウト、User・Bookモデル

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'cc81ce08ada7f1e4d6623cca0386927a8597e53e795445d1b0592d123ea38204bfa8e7c142f919bc3681a266af36ac1a0d3a7b3b93b44c9b557eaca0161acc15'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
-
+  get 'home/about' => 'homes#about', as: 'about_home'
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
 end
