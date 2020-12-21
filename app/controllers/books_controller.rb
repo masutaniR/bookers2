89af289cc5216974431ced2cd86b346f9d6bb850
masutaniR <popohu87@gmail.com>
2020-12-21 06:16:39 +0000

    [Update] 4-10 全体レイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,13 +12,15 @@ class BooksController < ApplicationController
       redirect_to book_path(@book.id), notice: 'You have created book successfully.'
     else
       @books = Book.all
+      @user = current_user
       render :index
     end
   end
 
   def show
-    @book = Book.find(params[:id])
-    @user = @book.user
+    @book = Book.new
+    @books = Book.find(params[:id])
+    @user = @books.user
   end
 
   def edit
@@ -47,7 +49,7 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title, :opinion)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_025936) do
+ActiveRecord::Schema.define(version: 2020_12_21_052211) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
-    t.text "opinion"
+    t.text "body"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
