06f78e3c001365e420806d76b63221b0d76f49fe
masutaniR <popohu87@gmail.com>
2020-12-22 03:54:14 +0000

    [Add] 4-10 sessionのリダイレクト設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,18 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
+  private
+
+  def after_sign_up_path_for(resource)
+    current_user
+  end
+
+  def after_sign_in_path_for(resource)
+    current_user
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
 end
